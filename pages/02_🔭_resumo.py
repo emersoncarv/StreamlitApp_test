ab49c0dbaf759ed1c4124d123fd592c19cef9c5b
Emerson Carvalho de Souza <emersoncarv@gmail.com>
2024-12-13 16:50:01 -0300

    Ajustando tabelas resumo

--- pages/02_🔭_resumo.py
+++ pages/02_🔭_resumo.py
@@ -5,11 +5,16 @@ st.header("Resumo dos Dados")
 if 'dados' not in st.session_state:
     st.error("Os dados não foram carregados")
 else:
-    dados = st.session_state['dados'].describe().reset_index()
-    st.write(dados)
+    # dados = st.session_state['dados'].describe().reset_index()
+    # st.write(dados)
 
-    # análise de N itens
     top_n = st.session_state.get('top_n', 10)
-    dados_brutos = dados
+    dados_brutos = st.session_state['dados']
     dados = dados_brutos.head(top_n)
-    st.write(dados)
+
+    st.write('Dados Completos')
+    st.write(dados_brutos.describe().reset_index())
+    
+    # análise de N itens
+    st.write(f'Primeiros {top_n} itens')
+    st.write(dados.describe().reset_index())
